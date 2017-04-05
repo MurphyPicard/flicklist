0e4d4f90629b664129f34eafa0728fe638501818
murphypicard <ayaghsizian@gmail.com>
2017-04-05 16:34:11 -0400

    deleted some spaces first commit

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,28 +1,20 @@
 
-
 var model = {
   watchlistItems: [],
   browseItems: []
-}
-
-
+};
 var api = {
-
   root: "https://api.themoviedb.org/3",
   token: "TODO", // TODO 0 add your api key
-
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
     // TODO 4b
     // implement this function
-
-    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg" 
+    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg";
   }
-}
-
-
+};
 /**
  * Makes an AJAX request to themoviedb.org, asking for some movies
  * if successful, updates the model.browseItems appropriately, and then invokes
@@ -41,10 +33,8 @@ function discoverMovies(callback) {
     }
   });
 }
-
-
 /**
- * Makes an AJAX request to the /search endpoint of the API, using the 
+ * Makes an AJAX request to the /search endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, updates model.browseItems appropriately and then invokes
@@ -63,52 +53,39 @@ function searchMovies(query, callback) {
     }
   });
 }
-
-
 /**
  * re-renders the page with new content, based on the current state of the model
  */
 function render() {
-
   // clear everything
   $("#section-watchlist ul").empty();
   $("#section-browse ul").empty();
-
   // insert watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-
-    // TODO 1 
+    // TODO 1
     // add an "I watched it" button and append it below the title
     // Clicking should remove this movie from the watchlist and re-render
-
     // TODO 2i
     // apply the classes "btn btn-danger" to the "I watched it button"
-
     // TODO 4a
-    // add a poster image and append it inside the 
+    // add a poster image and append it inside the
     // panel body above the button
-
     // TODO 2g
     // re-implement the li as a bootstrap panel with a heading and a body
     var itemView = $("<li></li>")
       .append(title)
       .attr("class", "item-watchlist");
-
     $("#section-watchlist ul").append(itemView);
   });
-
   // insert browse items
   model.browseItems.forEach(function(movie) {
-
     // TODO 2d continued
     // style this list item to look like the demo
     // You'll also need to make changes in index.html.
     // use the following BS classes:
-    // "list-group", "list-group-item", btn", "btn-primary", 
-
+    // "list-group", "list-group-item", btn", "btn-primary",
     var title = $("<h4></h4>").text(movie.original_title);
-
     var button = $("<button></button>")
       .text("Add to Watchlist")
       .click(function() {
@@ -116,22 +93,16 @@ function render() {
         render();
       })
       .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
-
     var overview = $("<p></p>").text(movie.overview);
-
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append(title)
       .append(overview)
       .append(button);
-      
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-  
 }
-
-
 // When the HTML document is ready, we call the discoverMovies function,
 // and pass the render function as its callback
 $(document).ready(function() {
